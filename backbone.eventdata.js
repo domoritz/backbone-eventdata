21f6172ac15cd3881f02fbaa7d9f98acff39809a
Dominik Moritz <domoritz@gmail.com>
2011-11-16 16:18:27 +0100

    Initial commit

---
+++ backbone.eventdata.js
@@ -0,0 +1,33 @@
+//= require backbone
+//= require jquery.hotkey
+
+/*
+ * reguires the jquery hotkey plugin by jresig
+ */
+
+(function(){
+	// Cached regex to split keys for `delegate`.
+	var eventSplitter = /^([\w:]+)(\[(.*)\])?\s*(.*)$/;
+
+	// Set up all inheritable **Backbone.View** properties and methods.
+	_.extend(Backbone.View.prototype, {
+		delegateEvents : function(events) {
+			if (!(events || (events = this.events))) return;
+			if (_.isFunction(events)) events = events.call(this);
+			$(this.el).unbind('.delegateEvents' + this.cid);
+			for (var key in events) {
+				var method = this[events[key]];
+				if (!method) throw new Error('Event "' + events[key] + '" does not exist');
+				var match = key.match(eventSplitter);
+				var eventName = match[1], eventData = match[3], selector = match[4];
+				method = _.bind(method, this);
+				eventName += '.delegateEvents' + this.cid;
+				if (selector === '') {
+					$(this.el).bind(eventName, method);
+				} else {
+					$(this.el).delegate(selector, eventName, eventData, method);
+				}
+			}
+		}
+	});
+})();
