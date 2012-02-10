74ecee82b58cac51e31e2dab5db4a52ffe2b62e1
Dominik Moritz <domoritz@gmail.com>
2012-02-10 21:35:51 +0100

    adaptated patch for Backbone 0.9.1 if you still use backbone 0.5.?, do not use this

--- backbone.eventdata.js
+++ backbone.eventdata.js
@@ -6,23 +6,32 @@
 
 	// Set up all inheritable **Backbone.View** properties and methods.
 	_.extend(Backbone.View.prototype, {
-		delegateEvents : function(events) {
-			if (!(events || (events = this.events))) return;
-			if (_.isFunction(events)) events = events.call(this);
-			$(this.el).unbind('.delegateEvents' + this.cid);
+		delegateEvents: function(events) {
+			if (!(events || (events = getValue(this, 'events')))) return;
+			this.undelegateEvents();
 			for (var key in events) {
-				var method = this[events[key]];
+				var method = events[key];
+				if (!_.isFunction(method)) method = this[events[key]];
 				if (!method) throw new Error('Event "' + events[key] + '" does not exist');
 				var match = key.match(eventSplitter);
 				var eventName = match[1], eventData = match[3], selector = match[4];
 				method = _.bind(method, this);
 				eventName += '.delegateEvents' + this.cid;
 				if (selector === '') {
-					$(this.el).bind(eventName, eventData, method);
+					this.$el.bind(eventName, eventData, method);
 				} else {
-					$(this.el).delegate(selector, eventName, eventData, method);
+					this.$el.delegate(selector, eventName, eventData, method);
 				}
 			}
 		}
 	});
+
+	// Helper function to get a value from a Backbone object as a property
+	// or as a function.
+	var getValue = function(object, prop) {
+		if (!(object && object[prop])) return null;
+		return _.isFunction(object[prop]) ? object[prop]() : object[prop];
+	};
+
 })();
+
